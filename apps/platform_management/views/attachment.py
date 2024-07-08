e0d0ac9cf1e2d7059ec389d3e182ba57b9b42f05
v_gqpgguo <v_gqpgguo@tencent.com>
2024-07-08 14:53:21 +0800

    Initial commit

---
+++ apps/platform_management/views/attachment.py
@@ -0,0 +1,36 @@
+# views.py
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+from django.http import FileResponse, Http404
+
+from apps.platform_management.models import Attachment
+from apps.platform_management.serialiers.attachment import AttachmentSerializer
+
+
+class FileUploadView(generics.CreateAPIView):
+    queryset = Attachment.objects.all()
+    serializer_class = AttachmentSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            attachment_instance = serializer.save()
+            return Response({
+                'id': attachment_instance.id,
+                'file_name': attachment_instance.file.name.split('/')[-1],
+            }, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FileDownloadView(generics.RetrieveAPIView):
+    queryset = Attachment.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        try:
+            attachment = self.get_object()
+            return FileResponse(attachment.file, as_attachment=True, filename=attachment.file.name.split('/')[-1])
+        except Attachment.DoesNotExist:
+            raise Http404
---
+++ common/permissions.py
@@ -0,0 +1,27 @@
+from rest_framework.permissions import BasePermission
+
+
+class SuperAdministratorPermission(BasePermission):
+    """超级管理员权限"""
+    def has_permission(self, request, view):
+        return True
+
+
+class ManageCompanyAdministratorPermission(BasePermission):
+    """管理公司管理员权限"""
+    def has_permission(self, request, view):
+        # return request.user and request.user.is_company_admin
+        return True
+
+
+class InstructorPermission(BasePermission):
+    """讲师权限"""
+    def has_permission(self, request, view):
+        # return request.user and request.user.is_instructor
+        return True
+
+
+class StudentPermission(BasePermission):
+    """学员权限"""
+    def has_permission(self, request, view):
+        return True
