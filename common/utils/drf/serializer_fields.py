45cf9c10fe71511c3a5c60ba0011ab5fd421448c
v_gqpgguo <v_gqpgguo@tencent.com>
2024-08-04 22:59:55 +0800

    删除password字段

--- common/utils/drf/serializer_fields.py
+++ common/utils/drf/serializer_fields.py
@@ -5,7 +5,7 @@ from common.utils.cipher import cipher
 
 class PasswordField(serializers.CharField):
     def to_internal_value(self, data):
-        return cipher.encrypt(data)
+        return cipher.encrypt(str(data))
 
     def to_representation(self, data):
-        return cipher.decrypt(data)
+        return cipher.decrypt(str(data))
--- common/utils/excel_parser/mapping.py
+++ common/utils/excel_parser/mapping.py
@@ -1,8 +1,7 @@
 INSTRUCTOR_EXCEL_MAPPING = {
-    "姓名": {"type": str, "value": "name"},
+    "姓名": {"type": str, "value": "username"},
     "电话": {"type": str, "value": "phone"},
     "邮箱": {"type": str, "value": "email"},
-    "登录密码": {"type": str, "value": "password"},
     "所在城市": {"type": str, "value": "city"},
     "所在公司": {"type": str, "value": "company"},
     "部门": {"type": str, "value": "department"},
@@ -15,13 +14,12 @@ ADMINISTRATOR_EXCEL_MAPPING = {
     "管理员名称": {"type": str, "value": "username"},
     "邮箱": {"type": str, "value": "email"},
     "手机": {"type": str, "value": "phone"},
-    "登录密码": {"type": str, "value": "password"},
-    "所属公司": {"type": str, "value": "manage_company"},
+    "所属公司": {"type": str, "value": "affiliated_manage_company_name"},
     "权限角色": {"type": str, "value": "role"},
 }
 
 CLIENT_STUDENT_EXCEL_MAPPING = {
-    "学员名称": {"type": str, "value": "name"},
+    "学员名称": {"type": str, "value": "username"},
     "性别": {"type": str, "value": "gender"},
     "身份证号": {"type": str, "value": "id_number"},
     "学历": {"type": str, "value": "education"},
@@ -30,5 +28,4 @@ CLIENT_STUDENT_EXCEL_MAPPING = {
     "所属客户公司": {"type": str, "value": "affiliated_client_company_name"},
     "部门": {"type": str, "value": "department"},
     "职位": {"type": str, "value": "position"},
-    "登录密码": {"type": str, "value": "password"},
 }
