82713fedbab3438aed024cd612bf061436487383
gqp <446105468@qq.com>
2024-08-16 10:42:15 +0800

    临时添加事件创建接口

--- apps/platform_management/serialiers/all_schedules.py
+++ apps/platform_management/serialiers/all_schedules.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 
+from apps.platform_management.models import Event
 
-class AllScheduleSerializer(serializers.Serializer):
+
+class AllScheduleListSerializer(serializers.Serializer):
     start_date = serializers.DateField()
     end_date = serializers.DateField()
+
+
+class AllScheduleCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = "__all__"
