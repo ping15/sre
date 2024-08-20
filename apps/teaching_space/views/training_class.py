3e0517b4d634fb0786bb9c05106cf915a49caa3b
v_gqpgguo <v_gqpgguo@tencent.com>
2024-08-20 18:49:10 +0800

    添加事件约束

--- apps/teaching_space/views/training_class.py
+++ apps/teaching_space/views/training_class.py
@@ -36,33 +36,3 @@ class TrainingClassModelViewSet(ModelViewSet):
                 event_type=Event.EventType.CLASS_SCHEDULE.value,
             )
         return response
-
-    # def list(self, request, *args, **kwargs):
-    #     mock_data = [
-    #         {
-    #             "name": "SRE 专家培训课(中级)-1期",
-    #             "status": "筹备中",
-    #             "student_count": "56",
-    #             "instructor": "nekzhang",
-    #         },
-    #         {
-    #             "name": "SRE 专家培训课(中级)-2期",
-    #             "status": "开课中",
-    #             "student_count": "42",
-    #             "instructor": "nekzhang",
-    #         },
-    #     ]
-    #     return Response(mock_data)
-
-    # def retrieve(self, request, *args, **kwargs):
-    #     mock_data = {
-    #         "name": "SRE 专家培训课(中级)-2期",
-    #         "status": "开课中",
-    #         "class_mode": "线下课",
-    #         "num_lessons": 12,
-    #         "start_date": "2024-05-10",
-    #         "assessment_method": "闭卷考试",
-    #         "certification": "无证",
-    #         "location": "广东省深圳市南山区科兴科学院D2栋",
-    #     }
-    #     return Response(mock_data)
