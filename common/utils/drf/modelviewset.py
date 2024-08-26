458ecad86507bcb50ebb38e3978fb229ace7f96b
gqp <446105468@qq.com>
2024-08-26 14:46:06 +0800

    处理培训班异常

--- common/utils/drf/modelviewset.py
+++ common/utils/drf/modelviewset.py
@@ -59,6 +59,17 @@ class ModelViewSet(DRFModelViewSet):
         serializer.is_valid(raise_exception=True)
         return serializer.validated_data
 
+    # def list(self, request, *args, **kwargs):
+    #     queryset = self.filter_queryset(self.get_queryset())
+    #
+    #     page = self.paginate_queryset(queryset)
+    #     if page is not None:
+    #         serializer = self.get_serializer(page, many=True)
+    #         return self.get_paginated_response(serializer.data)
+    #
+    #     serializer = self.get_serializer(queryset, many=True)
+    #     return Response(serializer.data)
+
     def retrieve(self, request, *args, **kwargs):
         return Response(super().retrieve(request, *args, **kwargs).data)
 
