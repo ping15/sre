fb1d72baa968e9ef38df4fe8334e7e7661fdbc5e
v_gqpgguo <v_gqpgguo@tencent.com>
2024-09-11 18:15:09 +0800

    前端index.html注入环境变量

--- home_application/views.py
+++ home_application/views.py
@@ -10,6 +10,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+import os
 
 from django.shortcuts import render
 
@@ -20,7 +21,10 @@ def home(request):
     """
     首页
     """
-    return render(request, "home_application/index.html")
+    context = {
+        "BK_API_HOST": os.environ.get("BK_API_HOST", "")
+    }
+    return render(request, "home_application/index.html", context=context)
 
 
 def dev_guide(request):
