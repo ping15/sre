3008d29b6ed1ab1a097af71a38e69e294d3f9b2c
v_gqpgguo <v_gqpgguo@tencent.com>
2024-09-24 09:59:56 +0800

    修改手机号校验

--- common/utils/drf/serializer_validator.py
+++ common/utils/drf/serializer_validator.py
@@ -20,9 +20,9 @@ class BasicSerializerValidator:
         Simple validation for phone number (example: 11 digits only).
         You can customize this as per your requirements.
         """
-        pattern = re.compile(r"^1[35678]\d{9}$")
+        pattern = re.compile(r"^1[356789]\d{9}$")
         if not pattern.match(value):
-            raise serializers.ValidationError("手机号必须为11位，第一位为1，第二位可选数字[3,5,6,7,8]")
+            raise serializers.ValidationError("手机号必须为11位，第一位为1，第二位可选数字[3,5,6,7,8,9]")
 
         return value
 
