d2f4a1f68e5621d663a8dccda713404acd189546
gqp <446105468@qq.com>
2024-09-30 17:15:42 +0800

    筛选修改

--- apps/platform_management/filters/client_approval_slip.py
+++ apps/platform_management/filters/client_approval_slip.py
@@ -5,12 +5,8 @@ from common.utils.drf.filters import BaseFilterSet
 
 class ClientApprovalSlipFilterClass(BaseFilterSet):
     id = django_filters.NumberFilter("id")
-    affiliated_manage_company_name = django_filters.CharFilter(
-        "affiliated_manage_company_name", lookup_expr="icontains"
-    )
-    affiliated_client_company_name = django_filters.CharFilter(
-        "affiliated_client_company_name", lookup_expr="icontains"
-    )
-    submitter = django_filters.CharFilter("submitter", lookup_expr="icontains")
-    status = django_filters.CharFilter("status", lookup_expr="icontains")
+    affiliated_manage_company_name = django_filters.CharFilter("affiliated_manage_company_name")
+    affiliated_client_company_name = django_filters.CharFilter("affiliated_client_company_name")
+    submitter = django_filters.CharFilter("submitter")
+    status = django_filters.CharFilter("status")
     submission_datetime = django_filters.DateTimeFromToRangeFilter("submission_datetime")
--- apps/teaching_space/filters/training_class.py
+++ apps/teaching_space/filters/training_class.py
@@ -4,6 +4,6 @@ from common.utils.drf.filters import BaseFilterSet, PropertyFilter
 
 
 class TrainingClassFilterClass(BaseFilterSet):
-    name = PropertyFilter("name", lookup_expr="icontains")
-    instructor_name = PropertyFilter("instructor_name", lookup_expr="icontains")
-    target_client_company = django_filters.NumberFilter("target_client_company_id")
+    name = PropertyFilter("name")
+    instructor_name = PropertyFilter("instructor_name")
+    target_client_company = django_filters.NumberFilter("target_client_company_id", lookup_expr="exact")
