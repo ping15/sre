274bfdd19a90293a463448ba9a728a23a718f0c3
gqp <446105468@qq.com>
2024-11-25 18:15:55 +0800

    prod settings添加考试系统数据库配置 # Reviewed, transaction id: 24746

--- config/prod.py
+++ config/prod.py
@@ -44,6 +44,19 @@ DATABASES = {
             'auth_plugin': 'mysql_native_password',
             'charset': 'utf8mb4',
         }
+    },
+    'exam-system': {
+        'ENGINE': 'django.db.backends.mysql',
+        # 'ENGINE': 'mysql.connector.django',
+        'NAME': os.getenv('EXAM_SYSTEM_DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': '3306',
+        'OPTIONS': {
+            'auth_plugin': 'mysql_native_password',
+            'charset': 'utf8mb4',
+        }
     }
 }
 
